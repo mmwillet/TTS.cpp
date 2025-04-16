66e5a4bbfba6b4509da5d5f69017d92e9a44c09e
Daniel Tang <danielzgtg.opensource@gmail.com>
2025-04-16 00:02:11 -0400

    playback: Switch to SDL2 for Win32/macOS support
    libpulse-simple was Linux-only. Switch to SDL2 for audio output, like
    how Whisper.cpp uses SDL2 for audio input. This allows for Windows and
    macOS support with a similar level of simplicity.

--- examples/cli/playback.cpp
+++ examples/cli/playback.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-#include <pulse/simple.h>
+#include <cstdint>
 #include "playback.h"
 
-#ifndef PULSE_INSTALL
+#ifndef SDL2_INSTALL
 void register_play_tts_response_args(arg_list & args) {
     // Hide --play
 }
@@ -11,6 +10,7 @@ bool play_tts_response(arg_list & args, const tts_response & data, float sample_
     return false;
 }
 #else
+#include "SDL.h"
 void register_play_tts_response_args(arg_list & args) {
     args.add_argument(bool_arg("--play", "(OPTIONAL) Whether to play back the audio immediately instead of saving it to file."));
 }
@@ -20,26 +20,43 @@ bool play_tts_response(arg_list & args, const tts_response & data, float sample_
         return false;
     }
 
-    const pa_sample_spec ss{
-        .format = PA_SAMPLE_FLOAT32NE,
-        .rate = static_cast<unsigned>(sample_rate),
+    if (SDL_Init(SDL_INIT_AUDIO)) {
+        fprintf(stderr, "SDL_INIT failed\n");
+        exit(1);
+    }
+
+    const SDL_AudioSpec desired{
+        .freq = static_cast<int>(sample_rate),
+        .format = AUDIO_F32,
         .channels = 1,
+        .silence = 0,
+        .padding = 0,
+        .size = static_cast<unsigned>(data.n_outputs),
+        .callback = nullptr,
+        .userdata = nullptr,
     };
-    pa_simple *s{pa_simple_new(
-        nullptr, "STT.cpp", PA_STREAM_PLAYBACK, nullptr,
-        "STT.cpp Text to speech", &ss, nullptr, nullptr, nullptr
-    )};
-    if (!s) {
-        std::cerr << "pa_simple_new failed" << std::endl;
+    const SDL_AudioDeviceID dev = SDL_OpenAudioDevice(nullptr, false, &desired, nullptr, 0);
+    if (!dev) {
+        fprintf(stderr, "SDL_OpenAudioDevice failed\n");
         exit(1);
     }
-    std::cout << "Playing audio: " << data.n_outputs << std::endl;
-    if (pa_simple_write(s, data.data, data.n_outputs * sizeof(data.data[0]), nullptr)) {
-        std::cerr << "pa_simple_write failed" << std::endl;
+
+    SDL_PauseAudioDevice(dev, false);
+    fprintf(stdout, "Playing %ld samples of audio\n", data.n_outputs);
+    if (SDL_QueueAudio(dev, data.data, data.n_outputs * sizeof(data.data[0]))) {
+        fprintf(stderr, "SDL_QueueAudio failed\n");
         exit(1);
     }
-    pa_simple_drain(s, nullptr);
-    pa_simple_free(s);
+
+    SDL_Event event;
+    while (SDL_GetQueuedAudioSize(dev)) {
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT) break;
+        SDL_Delay(100);
+    }
+
+    SDL_CloseAudioDevice(dev);
+    SDL_Quit();
+
     return true;
 }
 #endif
