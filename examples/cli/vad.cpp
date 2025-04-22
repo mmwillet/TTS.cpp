680084bbc62191a6e6f30025ec493ee6f07bebae
Matthew Willett-Jeffries <matthewwillett-jeffries@MacBookPro.lan>
2025-04-22 18:53:42 -0400

    adds earlier termination and reverse iteratio

--- examples/cli/vad.cpp
+++ examples/cli/vad.cpp
@@ -14,14 +14,18 @@ void apply_energy_voice_inactivity_detection(
 	int ms_per_frame,
 	int frame_threshold,
 	float normalized_energy_threshold,
-	int trailing_silent_frames) {
+	int trailing_silent_frames,
+	int early_cutoff_seconds_threshold,
+	float early_cutoff_energy_threshold) {
 	int samples_per_frame = (int) (ms_per_frame * sample_rate / 1000.0f);
 	int n_frames = (int) (data.n_outputs / samples_per_frame);
+	int early_cuttoff_frames = (int)((early_cutoff_seconds_threshold * 1000) / ms_per_frame);
 
 	// for min-max normalization
 	float max_energy = 0.0f;
 	float min_energy = 0.0f;
 	float * energies = (float *) malloc(n_frames * sizeof(float));
+	int silent_frames = 0;
 
 	// compute the energies and the necessary elements for min-max normalization
 	for (int i = 0; i < n_frames; i++) {
@@ -35,16 +39,26 @@ void apply_energy_voice_inactivity_detection(
 		} else if (energies[i] < min_energy) {
 			min_energy = energies[i];
 		}
+		if (energies[i] <= early_cutoff_energy_threshold) {
+			silent_frames++;
+		} else {
+			silent_frames = 0;
+		}
+		if (silent_frames >= early_cuttoff_frames) {
+			data.n_outputs = (i + trailing_silent_frames - silent_frames) * samples_per_frame;
+			free(energies);
+			return;
+		}
 	}
 
 	int concurrent_silent_frames = 0;
 
-	for (int i = 0; i < n_frames; i++) {
-		float frame_energy = (energies[i] - min_energy) / (max_energy - min_energy);
+	for (int i = n_frames; i > 0; i--) {
+		float frame_energy = (energies[i-1] - min_energy) / (max_energy - min_energy);
 		if (frame_energy < normalized_energy_threshold) {
 			concurrent_silent_frames++;
 		} else {
-			concurrent_silent_frames = 0;
+			break;
 		}
 	}
 	if (concurrent_silent_frames >= frame_threshold) {
--- examples/cli/vad.h
+++ examples/cli/vad.h
@@ -15,4 +15,7 @@ void apply_energy_voice_inactivity_detection(
 	int ms_per_frame = 10, // the audio time per frame
 	int frame_threshold = 20, // the number of trailing empty frames upon which silence is clipped.
 	float normalized_energy_threshold = 0.01f, // the normalized threshold to determine a silent frame
-	int trailing_silent_frames = 5);
+	int trailing_silent_frames = 5, // the number of frames of silence to allow
+	int early_cutoff_seconds_threshold = 3, // the number of seconds of complete silence before terminating and cutting audio early
+	float early_cutoff_energy_threshold = 0.1 // the energy threshold for treating a frame as silent for early cutoff
+);
